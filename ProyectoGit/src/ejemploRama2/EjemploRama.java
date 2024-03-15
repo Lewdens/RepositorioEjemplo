86080efae779259b9082c203aaaa48574019f415
Christian <Christian@DESKTOP-6Q1I5TG>
2024-03-15 13:58:53 +0100

    v1 (rama2)

---
+++ ProyectoGit/src/ejemploRama2/EjemploRama.java
@@ -0,0 +1,11 @@
+package ejemploRama2;
+
+public class EjemploRama {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Versión 1");
+		System.out.println("Localizado en la rama2");
+		
+	}
+}
